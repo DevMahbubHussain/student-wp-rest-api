83e1c51d5683bfb17a9526fa7fc56f924f548f9a
Mahbub <mahbubhussaincse@gmail.com>
2024-05-02 16:01:45 +0600

    Object Cache Implemented

--- includes/functions.php
+++ includes/functions.php
@@ -39,6 +39,8 @@ function mh_insert_student_infos($args = [])
             ],
             ['%d']
         );
+        // wp_cache_delete('student-' . $id, 'student');
+        mh_student_purge_cache($id);
         return $updated;
     } else {
         $format = [
@@ -55,6 +57,8 @@ function mh_insert_student_infos($args = [])
             return new \WP_Error('failed-to-insert', __('Failed to insert data', 'student-info'));
         }
 
+        // wp_cache_delete('count', 'student');
+        mh_student_purge_cache();
         return $wpdb->insert_id;
     }
 }
@@ -72,6 +76,11 @@ function mh_get_students($args = [])
     // $table = $wpdb->prefix . 'student_infos';
     $args = wp_parse_args($args, $defaults);
 
+    // cache
+    $last_changed = wp_cache_get_last_changed('student');
+    $key          = md5(serialize(array_diff_assoc($args, $defaults)));
+    $cache_key    = "all:$key:$last_changed";
+
     // Fetch all students data
     // $students = $wpdb->get_results($wpdb->prepare(
     //     "SELECT * FROM $table 
@@ -81,9 +90,7 @@ function mh_get_students($args = [])
     //     $args['number']
     // ));
 
-
     // return $students;
-
     $sql = $wpdb->prepare(
         "SELECT * FROM {$wpdb->prefix}student_infos
             ORDER BY {$args['orderby']} {$args['order']}
@@ -92,37 +99,70 @@ function mh_get_students($args = [])
         $args['number']
     );
 
-    $items = $wpdb->get_results($sql);
+    $items = wp_cache_get($cache_key, 'student');
+
+    if (false === $items) {
+        $items = $wpdb->get_results($sql);
+    }
 
     return $items;
 }
 
-
 // count how many student records 
-
 function mh_count_student_info()
 {
     global $wpdb;
     $table = $wpdb->prefix . 'student_infos';
-    return (int) $wpdb->get_var("SELECT count(id) from $table");
+
+    $count = wp_cache_get('count', 'student');
+    if (false === $count) {
+        $count = (int) $wpdb->get_var("SELECT count(id) from $table");
+        wp_cache_set('count', $count, 'student');
+    }
+
+    return $count;
 }
 
-
-
-
+// get single items
 function mh_get_student_info($id)
 {
     global $wpdb;
-    return $wpdb->get_row($wpdb->prepare("SELECT * FROM {$wpdb->prefix}student_infos WHERE id = %d", $id));
+    $student = wp_cache_get('student-' . $id, 'student');
+
+    if (false === $student) {
+        $student = $wpdb->get_row($wpdb->prepare("SELECT * FROM {$wpdb->prefix}student_infos WHERE id = %d", $id));
+    }
+    wp_cache_set('student-' . $id, $student, 'student');
+    return $student;
 }
 
 // delete student single entry 
 function mh_delete_student_info($id)
 {
     global $wpdb;
+    mh_student_purge_cache($id);
     return $wpdb->delete(
         $wpdb->prefix . 'student_infos',
         ['id' => $id],
         ['%d']
     );
+}
+
+/**
+ * Purge the cache for books
+ *
+ * @param  int $student_id
+ *
+ * @return void
+ */
+function mh_student_purge_cache($student_id = null)
+{
+    $group = 'student';
+
+    if ($student_id) {
+        wp_cache_delete('book-' . $student_id, $group);
+    }
+
+    wp_cache_delete('count', $group);
+    wp_cache_set('last_changed', microtime(), $group);
 }
